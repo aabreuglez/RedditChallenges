38dfc1d0b7f6beae274f4f575c7c67beece9e06d
aabreuglez <aabreuglez@gmail.com>
2016-02-10 20:10:57 +0000

    Beated #249
    Testing Netbeans projects

---
+++ EASY/#249 Playing the Stock Market/src/EasyPlayingTheStockMarket.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.List;
+import static java.util.stream.Collectors.toList;
+import java.util.stream.Stream;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Kotenoru
+ */
+public class EasyPlayingTheStockMarket {
+
+
+
+    /**
+     * @param args the command line arguments
+     */
+    private String _input1;
+    
+    public EasyPlayingTheStockMarket(String _input1) {
+        this._input1 = _input1;
+    }
+    public String getInput1() {
+        return _input1;
+    }
+
+    public void setInput1(String input1) {
+        this._input1 = input1;
+    }
+        
+    public String market(){
+        //For assertions
+        String devolver="";
+        
+        //Parsing string value to ArrayList of Double
+        List<Double> temp = Stream.of(_input1.split(" ")).map(Double::parseDouble).collect(toList());
+        ArrayList<Double> numbers = new ArrayList<Double>(temp);
+        
+        int min=0;
+        int max=0;
+        Double diff=9999999.99;
+        
+        /*The purpose is to get the mayor benefit, buy low, sell high
+        */
+        
+        for (int i=0; i < numbers.size(); i++){    
+            //We have to wait 1 turn per selling so i+2
+            for(int j = i+2; j < numbers.size(); j++){
+                if (numbers.get(i) - numbers.get(j) < diff){
+                    diff = numbers.get(i) - numbers.get(j);
+                    max = j;
+                    min = i;
+                }
+                
+                
+            }
+        }
+        devolver += numbers.get(min).toString();
+        devolver += " ";
+        devolver += numbers.get(max).toString();
+        return devolver;
+    }
+    
+}
---
+++ EASY/#249 Playing the Stock Market/test/EasyPlayingTheStockMarketTest.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Kotenoru
+ */
+public class EasyPlayingTheStockMarketTest {
+    
+    public EasyPlayingTheStockMarket uno;
+    public EasyPlayingTheStockMarket challenge;
+    
+    public EasyPlayingTheStockMarketTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        uno = new EasyPlayingTheStockMarket("19.35 19.30 18.88 18.93 18.95 19.03 19.00 18.97 18.97 18.98 ");
+        challenge = new EasyPlayingTheStockMarket("9.20 8.03 10.02 8.08 8.14 8.10 8.31 8.28 8.35 8.34 8.39 8.45 8.38 8.38 8.32 8.36 8.28 8.28 8.38 8.48 8.49 8.54 8.73 8.72 8.76 8.74 8.87 8.82 8.81 8.82 8.85 8.85 8.86 8.63 8.70 8.68 8.72 8.77 8.69 8.65 8.70 8.98 8.98 8.87 8.71 9.17 9.34 9.28 8.98 9.02 9.16 9.15 9.07 9.14 9.13 9.10 9.16 9.06 9.10 9.15 9.11 8.72 8.86 8.83 8.70 8.69 8.73 8.73 8.67 8.70 8.69 8.81 8.82 8.83 8.91 8.80 8.97 8.86 8.81 8.87 8.82 8.78 8.82 8.77 8.54 8.32 8.33 8.32 8.51 8.53 8.52 8.41 8.55 8.31 8.38 8.34 8.34 8.19 8.17 8.16 ");
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of Market method, of class EasyPlayingTheStockMarket.
+     */
+    @Test
+    public void testMarket() {
+        String expResult="18.88 19.03";
+        String result = uno.market();
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testMarket2() {
+        String expResult="8.03 9.34";
+        String result = challenge.market();
+        assertEquals(expResult, result);
+    }  
+}
